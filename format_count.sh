9e4ce7de60c9c11ed263c0f8ddb1aff4f4a7cc6c
Graythorn <litianhe17369410843@gmail.com>
2024-01-31 08:49:02 +0800

    modify format script

--- format_count.sh
+++ format_count.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
+# 检查输入参数
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 <folder_name>"
+    exit 1
+fi
+
+FOLDER_NAME=$1
+
 # 格式化
-clang-format -i `find include/ -type f -name *.hpp`
-clang-format -i `find src/ example/ test/ -type f -name *.cpp`
+clang-format -i `find $FOLDER_NAME/include/ -type f \( -name "*.h" -o -name "*.hpp" \)`
+clang-format -i `find $FOLDER_NAME/src/ $FOLDER_NAME/example/ $FOLDER_NAME/test/ -type f \( -name "*.c" -o -name "*.cc" -o -name "*.cpp" \)`
 
 # 统计代码行数
 cloc --git `git branch --show-current`
